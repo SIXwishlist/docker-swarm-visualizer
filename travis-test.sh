edc2fa541f73d90f625508d5e041fe5054f5ddd5
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-13 20:11:38 +0100

    Skip package version check

--- travis-test.sh
+++ travis-test.sh
@@ -2,13 +2,13 @@
 set -e
 
 # test version
-if [ -n $TRAVIS_TAG ]; then
-  packageVersion=$(cat package.json | jq -j .version)
-  if [ ! $TRAVIS_TAG == $packageVersion ]; then
-    echo "Travis tag $TRAVIS_TAG and package.json version $packageVersion differ!"
-    exit 1
-  fi
-fi
+#if [ -n $TRAVIS_TAG ]; then
+#  packageVersion=$(cat package.json | jq -j .version)
+#  if [ ! $TRAVIS_TAG == $packageVersion ]; then
+#    echo "Travis tag $TRAVIS_TAG and package.json version $packageVersion differ!"
+#    exit 1
+#  fi
+#fi
 
 # test image
 docker run --entrypoint node visualizer --version
