52beeec2aac6b4fbade939c649d419475bdcf5ba
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-13 16:37:19 +0100

    Speed up build

--- travis-build.sh
+++ travis-build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 set -e
 
-dockerfile=Dockerfile
-
 if [ $ARCH != "amd64" ]; then
   # prepare qemu
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
-  dockerfile=Dockerfile.$ARCH
 
   if [ $ARCH == "arm64" ]; then
     # prepare qemu binary
     docker create --name register hypriot/qemu-register
     docker cp register:qemu-aarch64 qemu-aarch64-static
-    dockerfile=Dockerfile.aarch64
   fi
 fi
 
+if [ -d tmp ]; then
+  docker rm build
+  rm -rf tmp
+fi
+
 # build image
-docker build -t visualizer -f $dockerfile .
+docker build -t build -f Dockerfile.amd64-build .
+docker create --name build build
+docker cp build:/app/ tmp/
+rm -rf tmp/node_modules
+docker build -t visualizer -f Dockerfile.$ARCH .
