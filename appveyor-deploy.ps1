e1488d01e0c2596caa593ed65f83499c28f5f404
Stefan Scherer <scherer_stefan@icloud.com>
2017-03-27 21:26:30 +0200

    Move docker login to yml to be in sync with travis.yml

--- appveyor-deploy.ps1
+++ appveyor-deploy.ps1
@@ -1,13 +1,5 @@
 $ErrorActionPreference = 'Stop';
 
-if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
-  Write-Host "No version tag detected. Skip publishing."
-  exit 0
-}
-
-Write-Host Starting deploy
-docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
-
 docker tag visualizer sealsystems/visualizer:windows-amd64-$env:APPVEYOR_REPO_TAG_NAME
 
 docker push sealsystems/visualizer:windows-amd64-$env:APPVEYOR_REPO_TAG_NAME
